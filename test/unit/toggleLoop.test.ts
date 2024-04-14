99f9e64f60b095f03560d6c06e9f898c4d7bcde4
Jens B <jens.bachmayer@gmx.de>
2024-04-14 20:45:55 +0200

    fix: toggleLopp tests

--- test/unit/toggleLoop.test.ts
+++ test/unit/toggleLoop.test.ts
@@ -1,20 +1,53 @@
-import { beforeAll, expect, it, suite } from 'vitest';
+import { beforeAll, expect, it, suite, vi } from 'vitest';
+
+import { sleep } from './helpers.ts';
 
 beforeAll(() => {
-	expect(pInst).not.toBeNull();
 	pInst.noCanvas();
-	pInst.loop();
+	pInst.draw = function () {};
 });
 
-suite('The method `toggleLoop` is extended', () => {
-	it('should alter the private property `_loop`', () => {
-		expect(pInst._loop).toBe(true);
+suite('The method `toggleLoop`', () => {
+	it('should start looping a sketch', async () => {
+		// get initial values
+		pInst.noLoop();
+		await sleep(200);
+
+		const spy = vi.spyOn(pInst, 'draw');
+		let frameCount = pInst.frameCount;
+
+		expect(pInst.isLooping()).toBe(false);
+		expect(spy).not.toHaveBeenCalled();
+		expect(spy.mock.calls.length).toEqual(pInst.frameCount);
+
+		// call the method and let it loop
 		pInst.toggleLoop();
-		expect(pInst._loop).toBe(false);
+		await sleep(200);
+
+		expect(pInst.isLooping()).toBe(true);
+		expect(spy).toHaveBeenCalled();
+		expect(spy.mock.calls.length).approximately(pInst.frameCount, 1);
+	});
+
+	it('should stop looping a sketch', async () => {
+		// get initial values
+		pInst.loop();
+		const spy = vi.spyOn(pInst, 'draw');
+
+		await sleep(200);
+		expect(pInst.isLooping()).toBe(true);
+		expect(spy).toHaveBeenCalled();
+
 		pInst.toggleLoop();
-		expect(pInst._loop).toBe(true);
+		await sleep(200);
+
+		const expectedCalls = spy.mock.calls.length;
+		await sleep(200);
+
+		expect(pInst.isLooping()).toBe(false);
+		expect(spy).toHaveBeenCalledTimes(expectedCalls);
 	});
 
 	// @TODO
-	// 'draw should have been called x times' ...
+	// 'should be handled automatically on resize events' ...
 });
